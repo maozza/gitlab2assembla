9ae68676a6d110066a4017b2a67c47e1394f5935
maoz <maoz@leadspace.com>
2017-10-03 18:13:19 +0300

    check if tikects not empty

--- assembla_push_webhook.php
+++ assembla_push_webhook.php
@@ -41,14 +41,11 @@ function request($url, $method, $headers, $data = 'none') {
     );
     $context = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { 
-        echo 'Error on request to $url';        
+    if ($result === FALSE) {
+        echo 'Error on request to $url';
     }
     return $result;
-
-    
 }
-
 
 function post_comment($ticket_id, $comment) {
     /*
@@ -61,18 +58,15 @@ function post_comment($ticket_id, $comment) {
         "ticket_comment" => array(
             "comment" => $comment
         )
-    );    
+    );
     return request($url, 'POST', $headers, json_encode($data));
 }
-
-
-
 
 /*
  * Handling gitlab webhook
  */
 
-function get_assembla_ticket_number($commits_array){
+function get_assembla_ticket_number($commits_array) {
     /*
      * Get ticket id from commit messages
      * get all commit from gitlab webhook
@@ -80,36 +74,35 @@ function get_assembla_ticket_number($commits_array){
      * return uniqe tikets numbers
      */
     $messages = '';
-    foreach ($commits_array as $commit){
-        $messages .= $commit->message;        
+    foreach ($commits_array as $commit) {
+        $messages .= $commit->message;
     }
-    preg_match_all('/\#[0-9]+/', $messages , $matches);
-    return str_replace('#','',array_unique($matches[0]));    
+    preg_match_all('/\#[0-9]+/', $messages, $matches);
+    return str_replace('#', '', array_unique($matches[0]));
 }
 
-
-
-function get_commits_info($data){
+function get_commits_info($data) {
     /*
      * format Gitlab webhook data into assembla comment format
      * get gitlab data
      * return Assembla comment format 
      */
-    $commits_str = '[[url:' . $data->data->project->homepage .'|' . $data->data->project->name .']]' . "\n";
-    foreach ($data->data->commits as $commit){
+    $commits_str = '[[url:' . $data->data->project->homepage . '|' . $data->data->project->name . ']]' . "\n";
+    foreach ($data->data->commits as $commit) {
         $commits_str .= "h3. Commit \n";
-        $commits_str .=  '[[url:'. $commit->url . '|' . end(explode('/',$commit->url )) . ']]' . "\n" ;
+        $commits_str .= '[[url:' . $commit->url . '|' . end(explode('/', $commit->url)) . ']]' . "\n";
         $commits_str .= "Author:" . $commit->author->name . "\n";
-        $commits_str .= "Message: " . $commit->message . "\n" ;        
-        $commits_str .=  "<pre><code>\n" . implode("\n",$commit->modified ) . "\n</code></pre>\n" ;
+        $commits_str .= "Message: " . $commit->message . "\n";
+        $commits_str .= "<pre><code>\n" . implode("\n", $commit->modified) . "\n</code></pre>\n";
         $commits_str .= "\n";
-        
     }
-    return $commits_str;       
+    return $commits_str;
 }
 
-
-foreach ( get_assembla_ticket_number($data->data->commits) as $ticket) {
-    $comment = get_commits_info($data);
-    post_comment($ticket, $comment);
+$tickets = get_assembla_ticket_number($data->data->commits);
+if (!empty($tickets)) {
+    foreach ($tickets as $ticket) {
+        $comment = get_commits_info($data);
+        post_comment($ticket, $comment);
+    }
 }
